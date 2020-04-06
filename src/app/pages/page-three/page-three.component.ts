37997852eacdf15cbb96c9a89d3b35ebc11866f8
Kostiantyn Shabaliev <kshab@softserveinc.com>
2020-04-06 12:48:51 +0300

    init pages

---
+++ src/app/pages/page-three/page-three.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-three',
+  templateUrl: './page-three.component.html',
+  styleUrls: ['./page-three.component.scss']
+})
+export class PageThreeComponent {
+  title = 'ng-time-tracker';
+}
