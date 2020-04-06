0572633a6ed3f05636f8581925075a45db68652f
Kostiantyn Shabaliev <kshab@softserveinc.com>
2020-04-06 13:39:09 +0300

    init routing

---
+++ src/app/core/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PageOneComponent } from '../pages/page-one/page-one.component';
+import { PageTwoComponent } from '../pages/page-two/page-two.component';
+import { PageThreeComponent } from '../pages/page-three/page-three.component';
+import { ReportComponent } from '../pages/report/report.component';
+
+const routes: Routes = [
+    {
+        path: '', redirectTo: 'page-one', pathMatch: 'full'
+    },
+    {
+        path: 'page-one', component: PageOneComponent
+    },
+    {
+        path: 'page-two', component: PageTwoComponent
+    },
+    {
+        path: 'page-three', component: PageThreeComponent
+    },
+    {
+        path: 'report', component: ReportComponent
+    }
+];
+
+
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/core/app.module.ts
+++ src/app/core/app.module.ts
@@ -4,14 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '../material/material.module';
+import { PageOneComponent } from '../pages/page-one/page-one.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PageTwoComponent } from '../pages/page-two/page-two.component';
+import { PageThreeComponent } from '../pages/page-three/page-three.component';
+import { ReportComponent } from '../pages/report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent,
+    PageTwoComponent,
+    PageThreeComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     MaterialModule
   ],
   providers: [],
